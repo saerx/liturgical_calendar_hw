bc16831dd6e066572e3181dab68997b12e99115f
saerx <saerlaith.robyn@gmail.com>
2021-01-01 19:28:56 +0000

    added more detail on feast days

--- src/components/DateItem.js
+++ src/components/DateItem.js
@@ -1,10 +1,18 @@
 const DateItem = ({date, season, season_week, celebrations, weekday}) => {
 
+    const feastDays = celebrations.map((feast) => {
+        return <li>
+            <h5>{feast.title}</h5>
+            <p>Colour: {feast.colour}</p>
+            <p>Rank: {feast.rank}, {feast.rank_num}</p>
+            </li>
+    })
+
     return (
         <div id='date-item'>
             <li>
-                <h3>{date}, in the season of {season}</h3>
-                <h4>{celebrations[0].title}</h4>
+                <h3>{date}</h3>
+                <ul>{feastDays}</ul>
             </li>
         </div>
 
