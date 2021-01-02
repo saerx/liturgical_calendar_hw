d7d5c62808bea7f20a0023899e2f2157603b6c3e
saerx <saerlaith.robyn@gmail.com>
2021-01-02 14:26:04 +0000

    added changeable state and input to date

--- src/components/MonthChanger.js
+++ src/components/MonthChanger.js
@@ -2,12 +2,21 @@ import {useEffect, useState} from 'react';
 
 const MonthChanger = () => {
 
-    const [month, setMonth] = useState([])
+    const [month, setMonth] = useState("");
+
+    const handleMonthChange = (event) => {
+        setMonth(event.target.value)
+    };
 
     return (
         <div id="date-changer">
-        <input type="month" placeholder="2021-01" ></input>
-        
+        <input type="month" 
+               placeholder="2021-01" 
+               min="2021-01"
+               max="2021-12"
+               value={month}
+               onChange={handleMonthChange}
+               />
          </div>
 
     )
