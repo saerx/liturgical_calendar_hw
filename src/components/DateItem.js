074a500544331e83410a9b7bc2640dd6bfe5cff8
saerx <saerlaith.robyn@gmail.com>
2021-01-03 11:47:25 +0000

    styling changes and added initial caps to api data without it

--- src/components/DateItem.js
+++ src/components/DateItem.js
@@ -1,7 +1,36 @@
 import FeastItem from './FeastItem'
 import './DateItem.css'
+import { findRenderedComponentWithType } from 'react-dom/test-utils';
 const DateItem = ({date, season, season_week, celebrations, weekday}) => {
     if (!celebrations) return null;
+
+    let dateColor = null;
+
+    if (season === "christmas" || season === "easter") {
+        dateColor = {
+            color: "white",
+        } 
+    } else if (season === "ordinary") {
+        dateColor = {
+            color: "green",
+        } 
+    } else if (season === "lent" || season === "advent") {
+        dateColor = {
+            color: "violet",
+        }
+    }
+
+    let capitalisedSeason = null
+
+    if (season === "ordinary") {
+        capitalisedSeason = "Ordinary Time"
+    } else {
+        capitalisedSeason = season.charAt(0).toUpperCase() + season.slice(1)
+    }
+
+    let capitalisedWeekday = null
+
+    capitalisedWeekday = weekday.charAt(0).toUpperCase() + weekday.slice(1)
 
     const feastDays = celebrations.map((feast, index) => {
         return <FeastItem 
@@ -13,16 +42,17 @@ const DateItem = ({date, season, season_week, celebrations, weekday}) => {
                 />
     })
 
-    const dateColor = {
-        color: celebrations[0].colour,
-    };
+    
+
+    
 
 
     return (
         <div id='date-item' style={dateColor}>
             <li>
                 <h3>{date}</h3>
-                <h4>Week {season_week} of {season} </h4>
+                <h4>{capitalisedSeason} </h4>
+                <h5 id="weekday">{capitalisedWeekday}, Week {season_week}</h5>
                 <ul>{feastDays}</ul>
             </li>
         </div>
