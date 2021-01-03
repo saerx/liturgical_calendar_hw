2b38312edcf7e3e3d1c99ebc5662df669f2a4296
saerx <saerlaith.robyn@gmail.com>
2021-01-03 19:26:19 +0000

    started working on chart, imported react googlge chart

---
+++ src/components/ChartPage.js
@@ -0,0 +1,13 @@
+import Chart from "react-google-charts"
+
+const ChartPage = () => {
+
+
+    return (
+        <>
+            <h1> insert chart here</h1>
+        </>
+    )
+}
+
+export default ChartPage;
